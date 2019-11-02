fix data normalization

data_processing.py
@@ -9,21 +9,29 @@ import sklearn.model_selection
 def diabetic_retinopathy(test_size=0.2):
     data = np.loadtxt("data/classification/messidor_features.arff", delimiter=',', skiprows=24)
     x, y = data[:, :18], data[:, 18]
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, random_state=0,
+                                                                                test_size=test_size)
 
     scaler = sklearn.preprocessing.StandardScaler()
-    x = scaler.fit_transform(x)
 
-    return sklearn.model_selection.train_test_split(x, y, random_state=0, test_size=test_size)
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    return x_train, x_test, y_train, y_test
 
 
 def credit_card_client(test_size=0.2):
     data = np.loadtxt("data/classification/default_credit_card_clients.csv", delimiter=',', skiprows=2)
     x, y = data[:, 1:24], data[:, 24]
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, random_state=0,
+                                                                                test_size=test_size)
 
     scaler = sklearn.preprocessing.StandardScaler()
-    x = scaler.fit_transform(x)
 
-    return sklearn.model_selection.train_test_split(x, y, random_state=0, test_size=test_size)
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    return x_train, x_test, y_train, y_test
 
 
 # =======================================REGRESSION============================================ #
@@ -31,8 +39,12 @@ def credit_card_client(test_size=0.2):
 def red_wine_quality(test_size=0.2):
     data = np.loadtxt("data/regression/wine_quality/winequality-red.csv", delimiter=';', skiprows=1)
     x, y = data[:, :11], data[:, 11]
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, random_state=0,
+                                                                                test_size=test_size)
 
     scaler = sklearn.preprocessing.StandardScaler()
-    x = scaler.fit_transform(x)
 
-    return sklearn.model_selection.train_test_split(x, y, random_state=0, test_size=test_size)
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    return x_train, x_test, y_train, y_test