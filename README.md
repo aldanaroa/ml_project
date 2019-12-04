4636c198454bc2b0d8a8aec273f4fb0d814a9dbf
Dung Do <dzung.do@fruitful.io>
2019-12-04 12:05:12 -0500

    cifar10 refine

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ml_project
 COMP6321 - Machine Learning Project
+
+## 1.Required libraries
+-   python3
+-   pandas, scikit-learn, pytorch, matplotlib
+
+## 2. Data setup
+  #### 2.1 Classifiers
+  #### 2.2 Regressors
+  #### 2.3 Classifier interpretability (CIFAR10)
+
+## 3. How to run
+
+
--- cifar_cnn.py
+++ cifar_cnn.py
@@ -7,10 +7,11 @@ import torch.nn as nn
 import json
 
 import cifar_dataprocess
-import cifar_models
+from cifar_models import CNN3Conv
+from cifar_models import CNN4Conv
 
 
-def epoch_search(cnn, x_train, x_cv, y_train, y_cv, epoch_min=1, epoch_max=100):
+def epoch_search(cnn, x_train_, x_cv_, y_train_, y_cv_, max_epoch=100):
     training_loss_ = []
     validation_loss_ = []
     learning_rate = 0.01
@@ -20,14 +21,14 @@ def epoch_search(cnn, x_train, x_cv, y_train, y_cv, epoch_min=1, epoch_max=100):
 
     print("Start training")
 
-    for epoch in range(epoch_min, epoch_max + 1):
+    for epoch in range(max_epoch + 1):
         running_loss = 0.0
         train_loss = 0.0
         num_batches = 0
         print("Epoch %d" % epoch)
-        for i in range(0, len(x_train), batch_size):
-            x_batch = x_train[i:i + batch_size].clone()  # Slice out a mini-batch of features
-            y_batch = y_train[i:i + batch_size].clone()  # Slice out a mini-batch of targets
+        for i in range(0, len(x_train_), batch_size):
+            x_batch = x_train_[i:i + batch_size].clone()  # Slice out a mini-batch of features
+            y_batch = y_train_[i:i + batch_size].clone()  # Slice out a mini-batch of targets
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -49,9 +50,9 @@ def epoch_search(cnn, x_train, x_cv, y_train, y_cv, epoch_min=1, epoch_max=100):
             val_loss = 0.0
             val_num_batches = 0
 
-            for j in range(0, len(x_cv), batch_size):
-                x_val = x_cv[j:j + batch_size]  # Slice out a mini-batch of features
-                y_val = y_cv[j:j + batch_size]  # Slice out a mini-batch of targets
+            for j in range(0, len(x_cv_), batch_size):
+                x_val = x_cv_[j:j + batch_size]  # Slice out a mini-batch of features
+                y_val = y_cv_[j:j + batch_size]  # Slice out a mini-batch of targets
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
@@ -79,15 +80,6 @@ def train_and_export(cnn, x_train, x_cv, y_train, y_cv, model_file, result_file,
         json.dump(result, f)
 
 
-def show_conv1_layer(model):
-    kernels = model.conv1.weight.detach()
-    fig, axarr = plt.subplots(1, kernels.size(0))
-    for idx in range(kernels.size(0)):
-        img = kernels[idx].detach().numpy().transpose(1, 2, 0) + 0.5
-        axarr[idx].imshow(img)
-    plt.show()
-
-
 def plot_loss(filename):
     with open(filename, 'r') as f:
         data = json.load(f)
@@ -108,22 +100,46 @@ def score(net, x, y):
         y_batch = y[i: i + batch_size]
         accuracy += sklearn.metrics.accuracy_score(y_batch, y_pred.cpu()) * (batch_size / len(x))
 
-    print("\tSimple cnn accuracy: {0:.2f}%".format(accuracy * 100))
+    print("\tTest accuracy: {0:.2f}%".format(accuracy * 100))
 
 
-x_train, x_cv, y_train, y_cv, x_test, y_test = cifar_dataprocess.aug_split()
+def evaluate():
+    print("------------------CIFAR10----------------------")
+    print("CIFAR10 data augmentation...")
+    x_train, x_cv, y_train, y_cv, x_test, y_test = cifar_dataprocess.aug_split()
 
-x_train_tensor = torch.tensor(x_train, dtype=torch.float)
-y_train_tensor = torch.tensor(y_train)
+    x_train_tensor = torch.tensor(x_train, dtype=torch.float)
+    y_train_tensor = torch.tensor(y_train)
 
-x_cv_tensor = torch.tensor(x_cv, dtype=torch.float)
-y_cv_tensor = torch.tensor(y_cv)
+    x_cv_tensor = torch.tensor(x_cv, dtype=torch.float)
+    y_cv_tensor = torch.tensor(y_cv)
 
-x_test_tensor = torch.tensor(x_test, dtype=torch.float)
-y_test_tensor = torch.tensor(y_test)
+    x_test_tensor = torch.tensor(x_test, dtype=torch.float)
+    y_test_tensor = torch.tensor(y_test)
 
-net = cifar_models.CNN4Conv()
-train_and_export(net, x_train_tensor, x_cv_tensor, y_train_tensor, y_cv_tensor,
-                 "model/cifar10_3conv.pth", "model/cifar10_3conv.json", 60)
+    print("Traing on CIFAR10 augmented data...")
+    net = CNN3Conv()
+    train_and_export(net, x_train_tensor, x_cv_tensor, y_train_tensor, y_cv_tensor,
+                     "model/cifar10_3conv.pth", "model/cifar10_3conv.json", 60)
 
-score(net, x_test_tensor, y_test_tensor)
+    score(net, x_test_tensor, y_test_tensor)
+
+
+# def show_activation():
+#
+#     net = CNN3Conv()
+#     net.load_state_dict(torch.load("model/model_3conv_reg_100epoch.pth", map_location="cpu"))
+#
+#     sample_size = 10
+#     x1, y1 = cifar_dataprocess.load_data_batch("data/cifar/data_batch_1")
+#     x = torch.tensor(x1[:10], dtype=torch.float)
+#
+#
+#
+#
+#     kernels = net.conv1.weight.detach()
+#     fig, axarr = plt.subplots(1, kernels.size(0))
+#     for idx in range(kernels.size(0)):
+#         img = kernels[idx].detach().numpy().transpose(1, 2, 0) + 0.5
+#         axarr[idx].imshow(img)
+#     plt.show()
